407bd4aeeb983e15015245d49b7ec63311b2d7c6
Daniel Lopes <viana.daniel02@gmail.com>
2019-01-06 11:28:36 -0300

    Adicionando comentários para alguns métodos

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,13 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-
+    /**
+     * Indica os atributos para definição em massa
+     */
     protected $fillable = ['name', 'birth', 'gender', 'classroom_id'];
 
+    /**
+     * Formatação da data de nascimento (Ocorre na hora de serialização)
+     */
     protected $casts = [
         'birth' => 'date:d/m/Y'
     ];
 
+    /**
+     * Define os atributos a não serem mostrados após a serialização
+     */
     protected $hidden = [
         'created_at',
         'updated_at'
