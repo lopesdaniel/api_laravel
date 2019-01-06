bd6bdd0536542394f315e842e12c0b134859aebe
Daniel Lopes <viana.daniel02@gmail.com>
2019-01-06 17:33:30 -0300

    Aplicando HATEOAS

--- app/Http/Resources/Student.php
+++ app/Http/Resources/Student.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Services\LinksGenerator;
 
 class Student extends JsonResource
 {
@@ -14,12 +15,20 @@ class Student extends JsonResource
      */
     public function toArray($request)
     {
+
+        $links = new LinksGenerator;
+
+        $links->addGet('self', route('students.show', $this->id));
+        $links->addPut('update', route('students.update', $this->id));
+        $links->addDelete('delete', route('students.destroy', $this->id));
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'birth' => $this->birth,
             'gender' => $this->gender,
-            'classroom' => new Classroom($this->classroom)
+            'classroom' => new Classroom($this->classroom),
+            'links' => $links->toArray()
         ];
     }
 }
